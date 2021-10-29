14ce0b4d0dd6f7154d9749bfa034484dc9f6f046
Samo <samofanci@gmail.com>
2021-10-29 14:30:35 +0200

    "data gathering tests"

--- scraper.py
+++ scraper.py
@@ -1,32 +1,84 @@
 REDDIT_CLIENT_ID = "BpU3gU0TUYg2Kj_c6QPVPg"
 REDDIT_SECRET_KEY = "jpMFV1SmlY9IBHtQZmBn5UhEOWdNWg" # not so secret
+STORAGE_FOLDER_NAME = "Data"
+REDDIT_DATA_FILENAME = "reddit.json"
 VERSION = "0.0.1"
 
-import requests
 import praw
-
-
+import json
+import datetime as dt
+from psaw import PushshiftAPI
+from os import path
 
 
 def start_reddit_api():
-    reddit_auth = requests.auth.HTTPBasicAuth(REDDIT_CLIENT_ID, REDDIT_SECRET_KEY)
-    password = str()
-
+    pwd = str()
     with open('pw.txt', 'r') as file:
-        password = file.read().strip()
+        pwd = file.read()
+    
+    reddit = praw.Reddit(
+        client_id = REDDIT_CLIENT_ID,
+        client_secret = REDDIT_SECRET_KEY,
+        password = pwd,
+        user_agent = "WSB_SCRAPER/{}".format(VERSION),
+        username = "ml_project"
+    )
 
-    data = {
-        'grant_type' : 'password',
-        'username' : 'jamescalam',
-        'password' : password
-    }
+    api = PushshiftAPI(reddit)
+    return api 
 
-    headers = { 'User-Agent' : 'Scraper ver {}'.format(VERSION)}
+def get_historic_posts(api, search_string, subreddit):
+    start_date = int(dt.datetime(2021, 1, 6).timestamp())
+    end_date = int(dt.datetime(2021, 1, 14).timestamp())
+    result = api.search_submissions(after=start_date,
+                           before=end_date,
+                           subreddit=subreddit,
+                           is_self=True,
+                           user_removed=False,
+                           mod_removed=False,
+                           q=search_string)
+    
+    return list(result)
 
-    token = requests.post("")
+def serialize_submissions(submissions):
+    out_dict = dict()
+    destination = path.join('.', STORAGE_FOLDER_NAME, REDDIT_DATA_FILENAME)
+    count = 0
+    for post in submissions:
+        if not (post.selftext == '[removed]' or post.selftext == '[deleted]'):
+            out_dict[post.id] = {
+                'score' : post.score,
+                'selftext' : post.selftext,
+                'title' : post.title,
+                'created_utc' : post.created_utc,
+                'upvote_ratio' : post.upvote_ratio,
+                'num_comments' : post.num_comments,
+            }
+
+            count += 1
+
+            try:
+                name = post.author.name
+                id = post.author.id
+
+                author = {
+                    'name' : name,
+                    'id' : id,
+                }
+                out_dict[post.id]['author'] = author
+            except:
+                continue
+
+
+    with open(destination, 'w') as file:
+        json.dump(out_dict, file, indent=4)
+
+    print("Saved {} Reddit posts".format(count))
 
 def main():
-    start_reddit_api()
+    api = start_reddit_api()
+    res = get_historic_posts(api, 'gme|gamestop', 'wallstreetbets')
+    serialize_submissions(res)
  
 
 if __name__ == "__main__":
