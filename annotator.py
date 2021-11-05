755c52d8a057456528fa45f596ad23be0afa3848
Samo <samofanci@gmail.com>
2021-11-05 20:25:14 +0100

    made a separate file with only annotated data

--- annotator.py
+++ annotator.py
@@ -4,6 +4,7 @@ from os import path
 
 DATA_FOLDER = "Data"
 
+
 def filter_post(post):
     res = re.findall("gme|gamestop", post['selftext'], re.IGNORECASE)
     if (len(res) > 1):
@@ -12,17 +13,31 @@ def filter_post(post):
     return False
 
 
-def annotate(n, filename):
+def format_json(json_file):
+    json_file.pop('irrelevant')
+    return json_file
+
+def save_progress(res : dict, dest_irr :str , dest_data : str):
+    with open(dest_irr, 'w') as file:
+        json.dump(res, file, indent=4)
+        file.close()
+
+    with open(dest_data, 'w') as file:
+        json.dump(format_json(res), file, indent=4)
+        file.close()
+
+def annotate(n: int, filename : str):
 
     filtered = 0
     with open(path.join(DATA_FOLDER, filename), 'r') as file:
         posts = json.load(file)
-        destination  = path.join(DATA_FOLDER, "reddit_annotated.json")
+        dest_irr = path.join(DATA_FOLDER, "reddit_annotated_with_irrelevant.json")
+        dest_data = path.join(DATA_FOLDER, 'reddit_annotated.json')
         res = {}
 
         # Resume if not done
-        if(path.isfile(destination)):
-            with open(destination, 'r') as file:
+        if(path.isfile(dest_irr)):
+            with open(dest_irr, 'r') as file:
                 res = json.load(file)
 
             file.close()
@@ -46,38 +61,33 @@ def annotate(n, filename):
             if not filter_post(posts[post]):
                 filtered += 1
                 continue
-            
+
             print()
             print(count)
             print("Filtered so far: {}".format(filtered))
             print("title:" + posts[post]['title'])
             print("text: " + posts[post]['selftext'])
-            value = input("Positive/Negative/Irrelevant/Pause = 1/0/3/pause : ")
+            value = input(
+                "Positive/Negative/Irrelevant/Pause = 1/0/3/pause : ")
             if value == "3":
                 if not post in res['irrelevant']:
                     res['irrelevant'][post] = 1
                 continue
 
             if value == "pause":
-                with open(destination, 'w') as file:
-                    json.dump(res, file, indent=4)
-
-                file.close()
+                save_progress(res, dest_irr, dest_data)
                 return
 
             count += 1
             res[post] = value
             print("====")
 
-    with open(destination, 'w') as file:
-        json.dump(res, file, indent=4)
-
-    file.close()
-
+    save_progress(res, dest_irr, dest_data)
 
 
 def main():
     annotate(500, "reddit.json")
+
 
 if __name__ == "__main__":
     main()
---
+++ model.py
@@ -0,0 +1,10 @@
+import sklearn
+import sklearn.preprocessing
+import sklearn.naive_bayes
+
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    main()
